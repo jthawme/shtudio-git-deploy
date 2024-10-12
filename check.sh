63d5793c6367acacd67f94f9777e5e4888e98f02
Jonny Thaw <hi@jthaw.me>
2024-10-12 15:56:39 +0100

    fix: maybe its it

--- check.sh
+++ check.sh
@@ -11,19 +11,19 @@ BRANCH=${4:-main}
 
 if [ -z "${GIT_REPO}" ];
 then
-    echo "No git repo assigned" | tee -a "/tmp/debug.log"
+    echo "No git repo assigned"
     exit 1 
 fi
 
 if [ -z "${LOCAL_FOLDER}" ];
 then
-    echo "No local repo assigned" | tee -a "/tmp/debug.log"
+    echo "No local repo assigned"
     exit 1 
 fi
 
 if [ -z "${PM2_NAME}" ];
 then
-    echo "No pm2 name assigned" | tee -a "/tmp/debug.log"
+    echo "No pm2 name assigned"
     exit 1 
 fi
 
@@ -31,16 +31,16 @@ LATEST_REMOTE=$(git ls-remote $GIT_REPO HEAD | cut -f1)
 LATEST_LOCAL=$(git --git-dir $LOCAL_REPO rev-parse HEAD);
 
 
-echo "LATEST REMOTE $LATEST_REMOTE" | tee -a "/tmp/debug.log"
-echo "LATEST LOCAL $LATEST_LOCAL" | tee -a "/tmp/debug.log"
+echo "LATEST REMOTE $LATEST_REMOTE"
+echo "LATEST LOCAL $LATEST_LOCAL"
 
 if [[ "$LATEST_REMOTE" != "$LATEST_LOCAL" ]];
 then
-    echo "New changes, running" | tee -a "/tmp/debug.log"
+    echo "New changes, running"
     # Commits dont match so presume there is new delicious content
     pm2 stop $PM2_NAME
     git -C $LOCAL_FOLDER pull origin $BRANCH
     npm --prefix $LOCAL_FOLDER ci $LOCAL_FOLDER
     pm2 restart $PM2_NAME
-    echo "New changes finished" | tee -a "/tmp/debug.log"
+    echo "New changes finished"
 fi
--- cron.sh
+++ cron.sh
@@ -3,22 +3,26 @@
 # A wrapper to load nvm and start keychain
 # to give the bash file access to node and the ssh agent
 
-date | tee -a "/tmp/debug.log"
-echo "TEST" | tee -a "/tmp/debug.log"
+date
+echo "TEST"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo $(node --version) | tee -a "/tmp/debug.log"
+echo $(node --version)
 
 # Use keychain to keep ssh-agent information available in a file
 /usr/bin/keychain $HOME/.ssh/id
 source $HOME/.keychain/${HOSTNAME}-sh
 
-echo "AGENT PID $SSH_AGENT_PID" | tee -a "/tmp/debug.log"
+echo "AGENT PID $SSH_AGENT_PID"
 
-./check.sh "$@"
+REL=$(dirname "$0")
+
+echo "$REL/check.sh"
+
+/usr/bin/bash $REL/check.sh "$@"
 
 
-echo "-------" | tee -a "/tmp/debug.log"
+echo "-------"

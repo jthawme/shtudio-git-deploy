3dc00f3bbed9a7ba1d3f51fe3039cf6dedffaa8c
Jonny Thaw <hi@jthaw.me>
2024-10-12 15:17:38 +0100

    fix: need more logggggging

--- check.sh
+++ check.sh
@@ -11,34 +11,37 @@ BRANCH=${4:-main}
 
 if [ -z "${GIT_REPO}" ];
 then
-    echo "No git repo assigned"
+    echo "No git repo assigned" | tee -a "/tmp/debug.log"
     exit 1 
 fi
 
 if [ -z "${LOCAL_FOLDER}" ];
 then
-    echo "No local repo assigned"
+    echo "No local repo assigned" | tee -a "/tmp/debug.log"
     exit 1 
 fi
 
 if [ -z "${PM2_NAME}" ];
 then
-    echo "No pm2 name assigned"
+    echo "No pm2 name assigned" | tee -a "/tmp/debug.log"
     exit 1 
 fi
 
 LATEST_REMOTE=$(git ls-remote $GIT_REPO HEAD | cut -f1)
 LATEST_LOCAL=$(git --git-dir $LOCAL_REPO rev-parse HEAD);
 
+
+echo "LATEST REMOTE $LATEST_REMOTE" | tee -a "/tmp/debug.log"
+echo "LATEST LOCAL $LATEST_LOCAL"
+ | tee -a "/tmp/debug.log"
+
 if [[ "$LATEST_REMOTE" != "$LATEST_LOCAL" ]];
 then
-    echo "New changes, running"
+    echo "New changes, running" | tee -a "/tmp/debug.log"
     # Commits dont match so presume there is new delicious content
     pm2 stop $PM2_NAME
     git -C $LOCAL_FOLDER pull origin $BRANCH
     npm --prefix $LOCAL_FOLDER ci $LOCAL_FOLDER
     pm2 restart $PM2_NAME
-    echo "New changes finished"
+    echo "New changes finished" | tee -a "/tmp/debug.log"
 fi
-
-date
--- cron.sh
+++ cron.sh
@@ -3,12 +3,22 @@
 # A wrapper to load nvm and start keychain
 # to give the bash file access to node and the ssh agent
 
+date | tee -a "/tmp/debug.log"
+echo "TEST" | tee -a "/tmp/debug.log"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo $(node --version) | tee -a "/tmp/debug.log"
 
 # Use keychain to keep ssh-agent information available in a file
 /usr/bin/keychain $HOME/.ssh/id
 source $HOME/.keychain/${HOSTNAME}-sh
 
+echo "AGENT PID $SSH_AGENT_PID" | tee -a "/tmp/debug.log"
+
 ./check.sh "$@"
+
+
+echo "-------" | tee -a "/tmp/debug.log"
